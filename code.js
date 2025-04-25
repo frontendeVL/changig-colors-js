e1f5636d004234e8600afe70980844dd1b7dbd94
Valeriia <alggv.b.2828@gmail.com>
2025-04-25 21:16:08 +0300

    Generating a random color in hexadecimal format

--- code.js
+++ code.js
@@ -1,11 +1,23 @@
 const gradientBox = document.querySelector(".gradient-box");
 const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
+const textarea = document.querySelector("textarea");
+const refreshBtn = document.querySelector("refresh");
 
-const generateGradient = () => {
+const getRandomColor = () => {
+  //Generating a random color in hexadecimal format
+  const randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+  return `#${randomHex}`;
+}
+
+const generateGradient = (isRandom) => {
+  if(isRandom){
+
+  }
   //Creating a gradient string using the select menu value with color input values
   const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
   gradientBox.style.background = gradient;
+  textarea.value = `background: ${gradient};`;
 }
 
 colorInputs.forEach(input => {
@@ -13,3 +25,5 @@ colorInputs.forEach(input => {
     input.addEventListener("input", generateGradient);
 });
 
+selectMenu.addEventListener("change", generateGradient);
+refreshBtn.addEventListener("click", () => generateGradient(true));
