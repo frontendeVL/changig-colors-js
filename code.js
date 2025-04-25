b02ce75b4ecd2af9cbf4d82ee9918304632c3c8f
Valeriia <alggv.b.2828@gmail.com>
2025-04-25 20:17:01 +0300

    Creating a gradient string using the select menu value with color input values

--- code.js
+++ code.js
@@ -1,10 +1,15 @@
+const gradientBox = document.querySelector(".gradient-box");
+const selectMenu = document.querySelector(".select-box select");
 const colorInputs = document.querySelectorAll(".colors input");
 
-const generateGradient = () =>{
-  console.log("Color update...");
+const generateGradient = () => {
+  //Creating a gradient string using the select menu value with color input values
+  const gradient = `linear-gradient(${selectMenu.value}, ${colorInputs[0].value}, ${colorInputs[1].value})`;
+  gradientBox.style.background = gradient;
 }
 
 colorInputs.forEach(input => {
-  //Calling generateGradient funcrion on each color input clicks
+  //Calling generateGradient function on each color input clicks
     input.addEventListener("input", generateGradient);
 });
+
